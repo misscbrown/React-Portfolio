5a430018e9d576e37711ca7e0867e493deb2db2b
cee breezy <djmisscbrown@hotmail.com>
2022-05-21 15:02:42 +0100

    added header area

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,20 @@
 import React from "react";
+import cBrownSmile from "../assets/images/cbrown-smiling.jpg"
 
 function Header() {
     return (
-      <main>
-       header
-      </main>
+      <section class="intro" id="home">
+      <h1 class="section__title section__title--intro">
+          Hey I'm <strong>Carla Brown</strong>
+      </h1>
+      
+      <img src={cBrownSmile} alt="hackathon" class="portfolio__img"/>
+  </section>
     );
   }
   
-  export default Header;
+export default Header;
+  
+
+
+{/* <p class="section__subtitle section__subtitle--intro">Junior Developer</p> */}
