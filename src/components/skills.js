89b0e2171c04db35170caea79b065d011c6cd49a
Thomas Buckley <tombuckley91@gmail.com>
2022-05-21 21:30:39 +0100

    Move carla branch contents to main

---
+++ src/components/skills.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Skills() {
+    return (
+      
+      <section class="my-services" id="services">
+          <h2 class="section__title section__title--services">What I do</h2>
+          <div class="services">
+              <div class="service">
+                  <h3>Full Stack Bootcamp</h3>
+                  <p>Currently studying a 6 month bootcamp at Birmingham University. Subjects covered 
+                      so far include Javascript, HTML, CSS, Bootstrap, Node, Mysql, Handlebars, API's, 
+                      local storage and much more.</p>  
+                  </div> 
+
+                  <div class="service">
+                      <h3>No Code Platforms</h3>
+                      <p>I have experience building websites using no-code platforms such as Wix and 
+                          Wordpress, designing and building responsive sites using plugins, templates 
+                          and media files.</p>  
+                      </div> 
+
+                  <div class="service">
+                      <h3>Creativity</h3>
+                      <p>Ability to use Canva to a high standard, studying Figma and UX and UI design in 
+                          my spare time in order to develop my front end skills to an excellent level. I 
+                          work on creative projects in my spare time.</p> 
+                  </div>  
+        </div> 
+        </section>
+    );
+  }
+  
+  export default Skills;
