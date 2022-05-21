fb97b45a2ff111f4408b22b290ad09d1403e17a5
cee breezy <djmisscbrown@hotmail.com>
2022-05-21 15:02:35 +0100

    added links

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,20 @@ import React from "react";
 
 function Footer() {
     return (
-      <main>
-       Footer
-      </main>
+      <footer class="footer">
+            <a href="mailto:djmisscbrown@hotmail.com" class="footer__link">Email Me!</a>
+            <ul class="social-list">
+          <li class="social-list__item"><a class="social-list__link"></a></li>
+            
+            <a href="https://www.linkedin.com/in/missc-brown-b6a255107/" class="footer__link">Linkedin</a>
+            <li class="social-list__item"><a class="social-list__link"></a>
+                
+
+                <a href="https://github.com/misscbrown" class="footer__link">Github</a></li>
+                <li class="social-list__item"><a class="social-list__link" 
+                href="https://drive.google.com/drive/folders/1Uk7j99Lv8e1fCdYpbW4_p92a5nuWpKhp?usp=sharing">View CV</a></li>      
+            </ul>
+        </footer>
     );
   }
   
