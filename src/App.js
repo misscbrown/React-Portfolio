1a3a3c042116ecdfdaeb9da1a9905d620a108dd8
cee breezy <djmisscbrown@hotmail.com>
2022-05-21 19:56:50 +0100

    fixed

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar'
+import About from './components/about'
+import Projects from './components/projects.js'
+import Skills from './components/skills'
+import Contact from './components/contact'
+import Footer from './components/footer'
+import Header from './components/header'
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Header />
+      <About id="about"/>
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
